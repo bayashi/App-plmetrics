18ded90e2f06b83903a66593c84b21c8e281b67d
bayashi <bayashi@cpan.org>
2013-06-16 20:58:36 +0900

    bumped version

--- lib/App/plmetrics.pm
+++ lib/App/plmetrics.pm
@@ -6,7 +6,7 @@ use Perl::Metrics::Lite;
 use Statistics::Swoop;
 use Text::ASCIITable;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 my %VIEWER = (
     qr/^modules?$/i => sub { $_[0]->_view_module($_[1]) },
